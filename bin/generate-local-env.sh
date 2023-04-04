b3291fb204c1037355a7bcb022822fa70534f2cd
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2023-04-04 13:50:49 +0200

    refactor: Generate local project env to .env.koala
    This prevents the overwrite of the usually used .env files in projects

--- bin/generate-local-env.sh
+++ bin/generate-local-env.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 pwd=$1
-cp $pwd/.env.dist $pwd/.env
+cp $pwd/.env.dist $pwd/.env.koala
 
 vars=$(printenv)
 
 for var in $vars; do
     if [[ $var == KOALA_* ]]; then
-        echo $var >> $pwd/.env
+        echo $var >> $pwd/.env.koala
     fi
 done
